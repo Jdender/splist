48127c5fe534eb8ce8884b5191564457950e171a
Jdender~ <jdenderplays@gmail.com>
2020-09-06 23:55:29 -0400

    Setup graphql client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,26 @@
 import { render } from 'react-dom';
 import React, { FC } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { useGetHelloQuery } from '../generated/graphql';
 
-const Index: FC = () => <h1>Hello world!</h1>;
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+});
+
+const Test: FC = () => {
+    const { data, loading, error } = useGetHelloQuery();
+
+    if (!data || loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
+    return <h1>Hello {data.hello}</h1>;
+};
+
+const Index: FC = () => (
+    <ApolloProvider client={client}>
+        <Test />
+    </ApolloProvider>
+);
 
 render(<Index />, document.getElementById('app'));
